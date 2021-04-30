8a96ff6b6b8586b482b1860150f935129a5e11ba
mine yiğiz <mineyigiz@hotmail.com>
2021-04-30 19:12:12 +0300

    KampOdev

---
+++ 2.günÖdev/src/CourseManager.java
@@ -0,0 +1,13 @@
+
+public class CourseManager {
+	public void registrationForTheCourse() {
+		
+		System.out.println("Kursa kayýt saðlandý");
+	}
+	
+	public void addCourse(Course course) {
+		
+		System.out.println(course.name + " hesabýnýza tanýmlandý.");
+	}
+
+}
