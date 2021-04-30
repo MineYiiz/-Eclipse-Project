93315ed6aaac67f39893efc5e8fd94062cc39afd
mine yiğiz <mineyigiz@hotmail.com>
2021-04-30 19:03:10 +0300

    kampOdev

---
+++ 2.günÖdev/src/Category.java
@@ -0,0 +1,18 @@
+
+public class Category {
+	
+	public Category() {
+		System.out.println("Kategorilerimiz");
+	}
+	
+	int id;
+	String name;
+	
+	
+	public Category(int id, String name) {
+		this.id = id;
+		this.name = name;
+		
+	}
+
+}
---
+++ 2.günÖdev/src/Course.java
@@ -0,0 +1,18 @@
+
+public class Course {
+	
+	public Course() {
+		System.out.println("Kurslarımız");
+	}
+	
+	int id;
+	String name;
+	String detail;
+	
+	public Course(int id, String name, String detail) {
+		this.id = id;
+		this.name = name;
+		this.detail = detail;
+	}
+
+}
